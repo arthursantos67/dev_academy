fd3b543d028471746a617e50af161d10ac80d570
arthur <fellipe.santos.700@ufrn.edu.br>
2025-12-05 19:31:09 -0300

    feat: configure API routing and include app urls

---
+++ src/academy/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StudentViewSet, CourseViewSet, EnrollmentViewSet
+
+router = DefaultRouter()
+router.register(r'students', StudentViewSet)
+router.register(r'courses', CourseViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
