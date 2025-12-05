ce7ee0fef689818bfae49373552b85dd268269aa
arthur <fellipe.santos.700@ufrn.edu.br>
2025-12-05 19:07:17 -0300

    feat: add viewsets for students, courses and enrollments

--- src/academy/views.py
+++ src/academy/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Student, Course, Enrollment
+from .serializers import StudentSerializer, CourseSerializer, EnrollmentSerializer
 
-# Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
