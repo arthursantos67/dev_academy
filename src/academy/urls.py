2669af8fc2cc3cdf52ac29244261a093925a8e19
arthur <fellipe.santos.700@ufrn.edu.br>
2025-12-06 00:15:38 -0300

    feat: configure API routing and endpoints

--- src/academy/urls.py
+++ src/academy/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, CourseViewSet, EnrollmentViewSet
+
+from .views import (
+    StudentViewSet,
+    CourseViewSet,
+    EnrollmentViewSet,
+    financial_report_api,
+)
 
 router = DefaultRouter()
-router.register(r'students', StudentViewSet)
-router.register(r'courses', CourseViewSet)
-router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'alunos', StudentViewSet)
+router.register(r'cursos', CourseViewSet)
+router.register(r'matriculas', EnrollmentViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+
+    path(
+        'api/relatorios/financeiro/',
+        financial_report_api,
+        name='api_relatorio_financeiro',
+    ),
 ]
